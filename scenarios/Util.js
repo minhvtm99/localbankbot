46d6568504a36a84cab837fcd63d9e60fffc4f8b
minhvtm99 <39894594+minhvtm99@users.noreply.github.com>
2018-07-05 13:59:43 +0700

    a

---
+++ scenarios/Util.js
@@ -0,0 +1,59 @@
+'use strict';
+ var request = require("request");
+
+class Util {
+	constructor() {
+		console.log('Scenario ATM starting...');
+	}
+
+	 // get tagged message
+    getMessageTags (msg_content) { 
+      var options = {
+        method: 'POST',
+        url: 'https://bankbotapi.herokuapp.com/message_categorize',
+        headers: {
+          'postman-token': '94080799-6b58-9785-2c2d-5e50ed758bcd',
+          'cache-control': 'no-cache',
+          'content-type': 'application/json'
+        },
+        body: {
+          message: msg_content
+        },
+        json: true
+      };
+		
+      this.getMyBody(options, function(err, body) {
+        if (err) {
+          return '';
+        } else {
+          let msg_tagged = body.categorized_msg;
+          return msg_tagged;
+     		 }
+    	});
+ 	}
+
+    getMyBody (options, callback) {
+	  request(options, function(error, response, body) {
+	    if (error || response.statusCode !== 200) {
+	      return callback(error || {
+	        statusCode: response.statusCode
+	      });
+	    } 
+	    callback(null, body);
+	  });
+	}
+
+	extractProperty(msg_tagged, property) {
+	  var result = '';
+	  var i;
+	  for (i = 0; i < msg_tagged.length; i++) {
+	    if (msg_tagged[i][1] === property) {
+	      result += msg_tagged[i][0] + ' ';
+	    }
+	  }
+	  console.log("Desired property: " + result);
+	  return result;
+	} 
+}
+
+module.exports = Util;
